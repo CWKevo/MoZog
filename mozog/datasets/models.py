cc0a14a5cd0d753c362960075e00d7ad04ec673b
Kevo <me@kevo.link>
2021-08-22 09:14:26 +0200

    Fixes

--- mozog/datasets/models.py
+++ mozog/datasets/models.py
@@ -1,4 +1,5 @@
 import typing as t
+
 import json
 
 from uuid import uuid4
@@ -32,20 +33,65 @@ class JSONField(TextField):
             return value
 
 
+
 class BaseModel(Model):
     identifier = CharField(max_length=36, null=False, unique=True, default=uuid4)
 
 
 
 class Intent(BaseModel):
-    requests: t.Container['Request']
-    responses: t.Container['Response']
+    priority = SmallIntegerField(null=False, default=3, choices=(
+        (1, "Very low"),
+        (2, "Low"),
+        (3, "Normal"),
+        (4, "High"),
+        (5, "Very high"),
+    ))
+
+    def to_json(self, simple: bool=False):
+        json = {
+            "identifier": self.identifier,
+            "priority": self.priority,
+        }
+
+        if not simple:
+            if self.requests:
+                json["requests"] = [request.to_json() for request in self.requests]
+
+            if self.responses:
+                json["responses"] = [response.to_json() for response in self.responses]
+
+
+        return json
+
+
+    requests: t.Iterator['Request']
+    responses: t.Iterator['Response']
 
 
 
 class Request(BaseModel):
     text = TextField() # type: str | TextField
-    intent = ForeignKeyField(Intent, backref="requests") # type: Intent | ForeignKeyField
+    intent = ForeignKeyField(Intent, backref="requests", null=True) # type: Intent | ForeignKeyField
+    priority = SmallIntegerField(null=False, default=3, choices=(
+        (1, "Very low"),
+        (2, "Low"),
+        (3, "Normal"),
+        (4, "High"),
+        (5, "Very high"),
+    ))
+
+
+    def to_json(self):
+        json = {
+            "identifier": self.identifier,
+            "text": self.text,
+            "priority": self.priority,
+            "intent": self.intent.to_json(True)
+        }
+
+        return json
+
 
 
 class RequestIndex(FTSModel):
@@ -60,7 +106,26 @@ class RequestIndex(FTSModel):
 
 class Response(BaseModel):
     text = TextField() # type: str | TextField
-    intent = ForeignKeyField(Intent, backref="responses") # type: Intent | ForeignKeyField
+    intent = ForeignKeyField(Intent, backref="responses", null=True) # type: Intent | ForeignKeyField
+    priority = SmallIntegerField(null=False, default=3, choices=(
+        (1, "Very low"),
+        (2, "Low"),
+        (3, "Normal"),
+        (4, "High"),
+        (5, "Very high"),
+    ))
+
+
+    def to_json(self):
+        json = {
+            "identifier": self.identifier,
+            "text": self.text,
+            "priority": self.priority,
+            "intent": self.intent.to_json(True)
+        }
+
+        return json
+
 
 
 class ResponseIndex(FTSModel):
--- mozog/mind/__init__.py
+++ mozog/mind/__init__.py
@@ -1,7 +1,8 @@
 import typing as t
 
-from ..datasets.models import Request, Response, RequestIndex, ResponseIndex, Intent
-from ..datasets.database import initialize_database
+from mozog.datasets.models import Request, Response, RequestIndex, ResponseIndex, Intent
+from mozog.datasets.database import initialize_database
+
 
 
 class Mozog:
@@ -42,14 +43,28 @@ class Mozog:
         return intent
 
 
-    def get_intent(self, request: t.Optional[str]=None, response: t.Optional[str]=None) -> t.Generator[Intent, None, None]:
-        _request: Request
-        _response: Response
-
+    def get_intent(self, request: t.Optional[str]=None, response: t.Optional[str]=None) -> t.Optional[t.Generator[Intent, None, None]]:
         if request is not None:
-            for _request in (Request.select().join(RequestIndex, on=(Request.id == RequestIndex.rowid)).where(RequestIndex.match(request)).order_by(RequestIndex.bm25())):
-                yield _request.intent
+            return (x.intent for x in Request.select().join(RequestIndex, on=(Request.id == RequestIndex.rowid)).where(RequestIndex.match(request)).order_by(Request.priority) if x.intent is not None)
+
+        elif response is not None:
+            return (x.intent for x in Response.select().join(ResponseIndex, on=(Response.id == ResponseIndex.rowid)).where(ResponseIndex.match(response)).order_by(Response.priority) if x.intent is not None)
 
         else:
-            for _response in (Response.select().join(ResponseIndex, on=(Response.id == ResponseIndex.rowid)).where(ResponseIndex.match(response)).order_by(ResponseIndex.bm25())):
-                yield _response.intent
+            raise TypeError("You must specify either `request` or `response`.")
+
+
+
+if __name__ == "__main__":
+    import json
+
+    mozog = Mozog("data.db")
+
+    request, _ = Request.get_or_create(text="Yes?")
+    request_2, _ = Request.get_or_create(text="Yes.", priority=2)
+    response, _ = Response.get_or_create(text="No.")
+
+    mozog.create_intent('yes.no', [request, request_2], [response])
+
+    for intent in mozog.get_intent(request="yes"):
+        print(json.dumps(intent.to_json(), indent=4))
